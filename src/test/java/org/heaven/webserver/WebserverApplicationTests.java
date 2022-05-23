e9c6c5f70a54b324dcb44be78a00ba1c4d44d2dc
kyoon3 <kyoon3@naver.com>
2022-05-23 19:26:28 +0900

    Added Default Spring Boot Setting

---
+++ src/test/java/org/heaven/webserver/WebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heaven.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
